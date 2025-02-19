080f11a2c7241c79de2d8764788902ef62667565
niclashogh <113596227+niclashogh@users.noreply.github.com>
2025-02-19 04:47:46 +0100

    Added razor.cs bc i like it more.

---
+++ BlazerWeb_Playground/BlazerWeb_Playground/Components/Pages/Paypal.razor.cs
@@ -0,0 +1,39 @@
+﻿using API_Playground.Services;
+using ModelLibrary;
+
+namespace BlazerWeb_Playground.Components.Pages
+{
+    public partial class Paypal // Manually added by creating new class: name.razor.cs
+    {
+        protected override void OnInitialized()
+        {
+            GetCatelog();
+        }
+
+        private ProductApiService productApiService = new();
+        private List<Product> catelog = new();
+        private List<Product> basket = new();
+
+        private async void GetCatelog()
+        {
+            catelog = await productApiService.GetCatelog();
+            this.StateHasChanged();
+        }
+
+        private void AddToBasket(int id)
+        {
+            Product? result = catelog.FirstOrDefault(sorting => sorting.Id == id);
+
+            if (result != null)
+            {
+                basket.Add(result);
+                this.StateHasChanged();
+            }
+        }
+
+        private void Buy()
+        {
+
+        }
+    }
+}
